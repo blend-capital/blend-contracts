9cb6889d7cedd18b43121a874ae14d6eae9d6674
Alex Mootz <alexmootz@hotmail.com>
2023-01-04 13:28:56 -0500

    chore: add emitter and backstop-module as dependencies to pool

--- lending-pool/src/dependencies/mod.rs
+++ lending-pool/src/dependencies/mod.rs
@@ -7,3 +7,13 @@ mod token;
 pub use token::Client as TokenClient;
 #[cfg(any(test, feature = "testutils"))]
 pub use token::{TokenMetadata, WASM as TOKEN_WASM};
+
+mod emitter;
+pub use emitter::Client as EmitterClient;
+#[cfg(any(test, feature = "testutils"))]
+pub use emitter::WASM as EMITTER_WASM;
+
+mod backstop;
+pub use backstop::Client as BackstopClient;
+#[cfg(any(test, feature = "testutils"))]
+pub use backstop::{BackstopDataKey, WASM as BACKSTOP_WASM};

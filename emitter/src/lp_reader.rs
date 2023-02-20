c1c786728e240a61685b6969dca85474ea412287
Alex Mootz <alexmootz@hotmail.com>
2023-02-20 17:55:55 -0500

    emitter: update to soroban 0.6.0

--- emitter/src/lp_reader.rs
+++ emitter/src/lp_reader.rs
@@ -1,12 +1,11 @@
-use soroban_auth::Identifier;
-use soroban_sdk::Env;
+use soroban_sdk::{Address, Env};
 
 // TODO: Fill out these functions once we have a liquidity pool implementation
 pub fn get_lp_share_value(e: &Env) -> i128 {
     1
 }
 
-pub fn get_lp_blend_holdings(e: &Env, holder: Identifier) -> i128 {
+pub fn get_lp_blend_holdings(e: &Env, holder: Address) -> i128 {
     let share_value = get_lp_share_value(e);
     let share_holdings = 100;
     share_value * share_holdings
--- emitter/tests/test_swap_backstop.rs
+++ emitter/tests/test_swap_backstop.rs
@@ -1,7 +1,6 @@
 #![cfg(test)]
 
-use soroban_auth::{Identifier, Signature};
-use soroban_sdk::{testutils::Accounts, Env, Status};
+use soroban_sdk::{testutils::Address as AddressTestTrait, Address, Env, Status};
 
 mod common;
 use crate::common::{create_token, create_wasm_emitter, generate_contract_id, EmitterError};
@@ -10,39 +9,21 @@ use crate::common::{create_token, create_wasm_emitter, generate_contract_id, Emi
 fn test_swap_backstop() {
     let e = Env::default();
 
-    let bombadil = e.accounts().generate_and_create();
-    let bombadil_id = Identifier::Account(bombadil.clone());
-
-    let backstop = generate_contract_id(&e);
-    let backstop_id = Identifier::Contract(backstop.clone());
-
-    let new_backstop = generate_contract_id(&e);
-    let new_backstop_id = Identifier::Contract(new_backstop.clone());
-
-    let (blend_id, blend_client) = create_token(&e, &bombadil_id);
+    let backstop = Address::random(&e);
+    let new_backstop = Address::random(&e);
 
     let blend_lp = generate_contract_id(&e);
 
-    let (emitter, emitter_client) = create_wasm_emitter(&e);
-    let emitter_id = Identifier::Contract(emitter.clone());
+    let (emitter_bytes, emitter_client) = create_wasm_emitter(&e);
+    let emitter = Address::from_contract_id(&e, &emitter_bytes);
+    let (blend_id, blend_client) = create_token(&e, &emitter);
     emitter_client.initialize(&backstop, &blend_id, &blend_lp);
 
-    // Mint Backstop Blend
-    blend_client
-        .with_source_account(&bombadil)
-        .mint(&Signature::Invoker, &0, &backstop_id, &100);
-    // Mint new Backstop Blend - Note: we mint 104 here just to check we're dividing raw Blend balance by 4
-    blend_client.with_source_account(&bombadil).mint(
-        &Signature::Invoker,
-        &0,
-        &new_backstop_id,
-        &104,
-    );
+    // mint backstop blend
+    blend_client.mint(&emitter, &backstop, &100);
 
-    // Set emitter as blend admin
-    blend_client
-        .with_source_account(&bombadil)
-        .set_admin(&Signature::Invoker, &0, &emitter_id);
+    // mint new backstop blend - NOTE: we mint 104 here just to check we're dividing raw Blend balance by 4
+    blend_client.mint(&emitter, &new_backstop, &104);
 
     let result = emitter_client.try_swap_bstop(&new_backstop);
 
@@ -59,37 +40,21 @@ fn test_swap_backstop() {
 fn test_swap_backstop_fails_with_insufficient_blend() {
     let e = Env::default();
 
-    let bombadil = e.accounts().generate_and_create();
-    let bombadil_id = Identifier::Account(bombadil.clone());
-
-    let backstop = generate_contract_id(&e);
-    let backstop_id = Identifier::Contract(backstop.clone());
-
-    let new_backstop = generate_contract_id(&e);
-    let new_backstop_id = Identifier::Contract(new_backstop.clone());
-
-    let (blend_id, blend_client) = create_token(&e, &bombadil_id);
+    let backstop = Address::random(&e);
+    let new_backstop = Address::random(&e);
 
     let blend_lp = generate_contract_id(&e);
 
-    let (emitter, emitter_client) = create_wasm_emitter(&e);
-    let emitter_id = Identifier::Contract(emitter.clone());
+    let (emitter_bytes, emitter_client) = create_wasm_emitter(&e);
+    let emitter = Address::from_contract_id(&e, &emitter_bytes);
+    let (blend_id, blend_client) = create_token(&e, &emitter);
     emitter_client.initialize(&backstop, &blend_id, &blend_lp);
-    //Mint Blend to Backstop
-    blend_client
-        .with_source_account(&bombadil)
-        .mint(&Signature::Invoker, &0, &backstop_id, &100);
-    //Mint Blend to new backstop - Note: we mint 103 here just to check we're dividing raw Blend balance by 4
-    blend_client.with_source_account(&bombadil).mint(
-        &Signature::Invoker,
-        &0,
-        &new_backstop_id,
-        &103,
-    );
-    //Set emitter as Blend admin
-    blend_client
-        .with_source_account(&bombadil)
-        .set_admin(&Signature::Invoker, &0, &emitter_id);
+
+    // mint backstop blend
+    blend_client.mint(&emitter, &backstop, &100);
+
+    // mint new backstop blend - NOTE: we mint 103 here just to check we're dividing raw Blend balance by 4
+    blend_client.mint(&emitter, &new_backstop, &103);
 
     let result = emitter_client.try_swap_bstop(&new_backstop);
 

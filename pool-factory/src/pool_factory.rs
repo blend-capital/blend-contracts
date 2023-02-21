6b180d33b873f9997b8dca45fbe23bad9f29ce6b
Alex Mootz <alexmootz@hotmail.com>
2023-02-21 09:15:17 -0500

    pool-factory: update to soroban 0.6.0

--- pool-factory/src/pool_factory.rs
+++ pool-factory/src/pool_factory.rs
@@ -1,4 +1,4 @@
-use crate::storage::{PoolFactoryStore, StorageManager};
+use crate::storage;
 use soroban_sdk::{contractimpl, symbol, BytesN, Env, RawVal, Symbol, Vec};
 
 pub struct PoolFactory;
@@ -29,37 +29,33 @@ pub trait PoolFactoryTrait {
 #[contractimpl]
 impl PoolFactoryTrait for PoolFactory {
     fn initialize(e: Env, wasm_hash: BytesN<32>) {
-        let storage = StorageManager::new(&e);
-        if storage.has_wasm_hash() {
+        if storage::has_wasm_hash(&e) {
             panic!("already initialized");
         }
-        storage.set_wasm_hash(wasm_hash);
+        storage::set_wasm_hash(&e, &wasm_hash);
     }
 
     fn deploy(e: Env, init_function: Symbol, args: Vec<RawVal>) -> BytesN<32> {
-        let storage = StorageManager::new(&e);
         let mut salt: [u8; 32] = [0; 32];
         let sequence_as_bytes = e.ledger().sequence().to_be_bytes();
-
         for n in 0..sequence_as_bytes.len() {
             salt[n] = sequence_as_bytes[n];
         }
 
         let pool_address = e
             .deployer()
-            .with_current_contract(salt)
-            .deploy(storage.get_wasm_hash());
+            .with_current_contract(&salt)
+            .deploy(&storage::get_wasm_hash(&e));
         // e.invoke_contract::<RawVal>(&pool_address, &init_function, args);
-        storage.set_deployed(pool_address.clone());
-        e.events().publish(
-            (symbol!("Pool"), symbol!("Factory"), symbol!("Deployed")),
-            pool_address.clone(),
-        );
+
+        storage::set_deployed(&e, &pool_address);
+
+        e.events()
+            .publish((symbol!("deploy"),), pool_address.clone());
         pool_address
     }
 
     fn is_pool(e: Env, pool_address: BytesN<32>) -> bool {
-        let storage = StorageManager::new(&e);
-        storage.is_deployed(pool_address)
+        storage::is_deployed(&e, &pool_address)
     }
 }
